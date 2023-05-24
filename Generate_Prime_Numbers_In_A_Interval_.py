cdf276111ab0abcb4c32506dbf3117e4e339582f
21P31A04L8 <100029361+21P31A04L8@users.noreply.github.com>
2023-05-24 15:12:59 +0530

    def p(n):     for i in range(2,n):         if n%i==0:             return False     else:         return True x=int(input()) y=int(input()) for i in range(x,y+1):     if i>1:         if p(i):             print(i)

---
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -0,0 +1,12 @@
+def p(n):
+    for i in range(2,n):
+        if n%i==0:
+            return False
+    else:
+        return True
+x=int(input())
+y=int(input())
+for i in range(x,y+1):
+    if i>1:
+        if p(i):
+            print(i)
